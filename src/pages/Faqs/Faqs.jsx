bbfa642e340933774298b7a83747178a76c7db20
Aryan-Jagadale <aryansjagadale@gmail.com>
2022-04-13 10:15:10 +0530

    Faqs changes as well as contact

--- src/pages/Faqs/Faqs.jsx
+++ src/pages/Faqs/Faqs.jsx
@@ -8,58 +8,116 @@ const Faqs = () => {
   };
   return (
     <>
-    <div className="page" style={faqStyle}>
-      <h2 className={styles.faqs}>Frequently Asked Questions</h2>
+      <div className="page" style={faqStyle}>
+        <h2 className={styles.faqs}>Frequently Asked Questions</h2>
 
-      <h2 className={styles.category}>About TEDxVITPune</h2>
+        <h2 className={styles.category}>About TEDxVITPune</h2>
 
-      <Accordian question="What is TEDx?" answer="TEDxVITPune is an event where live talks with unique ideas and performances are shared with the community to educate and engage them." />
-      {/*<Accordian question="How can I reach out to the organising team of TEDxVITPune?" 
+        <Accordian
+          question="What is TEDxVITPune?"
+          answer="TEDxVITPune is an event where live talks with unique ideas and performances are shared with the community to educate and engage them.
+      "
+        />
+        {/*<Accordian question="How can I reach out to the organising team of TEDxVITPune?" 
       answer="
   <p style=color:red>You can reach out to us through our Social Media handles</p>" />*/}
 
-      <Accordian question="How can I reach out to the organising team of TEDxVITPune?" 
-      answer="
-      <p>You can reach out to us through our Social Media handles</p>" />
+        <Accordian
+          question="How can I reach out to the organising team of TEDxVITPune?"
+          answer="
+      <p>You can reach out to us through our Social Media handles</p>
+      Check out our Contact page to know more about our endeavours and reach out to the organising team.
+      
+      "
+        />
 
-      <Accordian question="Where can I see previous TEDxVITPune talks?" 
-      // eslint-disable-next-line no-template-curly-in-string
-      answer="
+        <Accordian
+          question="Where can I see previous TEDxVITPune talks?"
+          // eslint-disable-next-line no-template-curly-in-string
+          answer="
       You can find talks from our previous speakers on the TEDx YouTube Channel.
       <br/>
       <a style=`text-decoration:none;color:red;font-weight: 600;` href=`${https://youtube.com/playlist?list=PL0687I2dEWcksr9RextzeIRFms8yjaKQi}`>2015, </a>
-      " />
+      "
+        />
 
-      <Accordian question=" Is TEDxVITPune a part of TED?" answer="No, it is an independently organized TEDx event licensed by TED. " />
-      <Accordian question="How do I suggest a Speaker for TEDxVITPune?" answer="
+        <Accordian
+          question=" Is TEDxVITPune a part of TED?"
+          answer="No, it is an independently organised TEDx event licensed by TED. "
+        />
+
+        <Accordian
+          question="How do I suggest a Speaker for TEDxVITPune?"
+          answer="
       If you’d like to nominate someone to be a speaker at TEDxVITPune, you can fill out our <a href=`{https://tedxvitpune.in/speaker-nomination}`><b>Speaker Nomination Form</b></a>  with a few details about the speaker and their ideas.
-      " />
+      "
+        />
 
-      <h2 className={styles.category}>Event specific queries</h2>
+        <h2 className={styles.category}>Event specific queries</h2>
 
-      <Accordian question="What is the nature of the event?" answer="Due to current covid restrictions, TEDxVITPune 2021 will be held in online mode on 29th May, 2021. The platform details will be shared with the attendees a week prior to the event." />
+        <Accordian
+          question="What is the nature of the event?"
+          answer="TEDxVITPune 2022 will be held in an offline mode. Other event-specific details will be shared with the attendees a week before the event.
+      "
+        />
 
-      <Accordian question="Is there a limit to the number of attendees?" answer="Yes, according to the guidelines set by TED, TEDxVITPune has a limit of 100 attendees." />
+        <Accordian
+          question="Is there a limit to the number of attendees?"
+          answer="Yes, according to the guidelines set by TED, TEDxVITPune has a limit of 100 attendees.
+      "
+        />
 
-      <Accordian question="What all is included in the ticket for TEDxVITPune?" answer="The event ticket includes access to all speaker talks and performances, networking activities, workshops, speaker interaction and specially curated merchandise delievered to your doorstep." />
+        <Accordian
+          question="Is TEDxVITPune a paid event? "
+          answer="Yes. We, here at TEDx, believe in a holistic experience that lasts a lifetime. From workshops to event merchandise, we promise to put forth an invaluable experience for all the attendees.
+      "
+        />
 
-      <Accordian question="What is the itinerary for the event?" answer="TEDxVITPune is a one-day event, the itinerary for which will be intimated to the attendees via email before the event." />
-      
-      <Accordian question="Is the ticket refundable or transferable?" answer="The ticket to TEDxVITPune is non-transferable and non-refundable." />
+        <Accordian
+          question="What all is included in the ticket for TEDxVITPune?"
+          answer="The event ticket includes access to all speaker talks and performances, networking activities, workshops, speaker interaction, and specially curated merchandise for all the attendees."
+        />
 
-      <Accordian question="How do I buy a ticket from Townscript?" answer="You can purchase a ticket to the event via Townscript after filling all the necessary details.
-      Buy ticket" />
+        <Accordian
+          question="What is the itinerary for the event?"
+          answer="TEDxVITPune is a one-day event, the itinerary for which will be intimated to the attendees via email before the event."
+        />
 
-      <Accordian question="What is the duration of the event?" answer="TEDxVITPune is a one-day event. The timings and itinerary will be notified to the selected attendees prior to the event." />
+        <Accordian
+          question="Is the ticket refundable or transferable?
+      "
+          answer="The ticket to TEDxVITPune is non-transferable and non-refundable."
+        />
 
-      <Accordian question=" Can I pick experience sessions to attend? " answer="The ticket includes entry to not just one but all the experience sessions. The attendee is free to take benefit of all the networking activities and workshops." />
-      
+        <Accordian
+          question="How do I buy a ticket from Townscript?"
+          answer="Upon the selection of your attendee application, you can purchase your ticket to the event via Townscript after filling in all the necessary details.
+      Apply as an attendee here!
+      "
+        />
 
+        <Accordian
+          question="What is the duration of the event?"
+          answer="TEDxVITPune is a one-day event. The timings and itinerary will be notified to the selected attendees prior to the event."
+        />
 
+        <Accordian
+          question="Who can attend the offline event?"
+          answer="Anyone interested can register by filling out an Attendee Application Form. However, only top 100 forms will be selected for further attending the event."
+        />
 
-    </div>
+        <Accordian
+          question="Are there any experience activities included along with the talks?"
+          answer="Yes! We have a host of experience activities planned for our attendees.
+      "
+        />
+
+        <Accordian
+          question=" Can I pick experience sessions to attend? "
+          answer="The ticket includes entry to not just one but all the experience sessions. The attendee is free to take benefit of all the networking activities and workshops."
+        />
+      </div>
     </>
-
   );
 };
 

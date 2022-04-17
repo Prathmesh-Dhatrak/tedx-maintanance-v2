bef940037dff63305a31e31e52e93993af5f8572
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2022-04-17 15:17:33 +0530

    TimeLine fix

--- src/images/Home/eventSVG/index.js
+++ src/images/Home/eventSVG/index.js
@@ -4,4 +4,5 @@ export { default as Event17 } from "./17.svg";
 export { default as Event18 } from "./18.svg";
 export { default as Event19 } from "./19.svg";
 export { default as Event21 } from "./21.svg";
-export { default as Event22 } from "./17.svg";
+export { default as Event20 } from "./17.svg";
+

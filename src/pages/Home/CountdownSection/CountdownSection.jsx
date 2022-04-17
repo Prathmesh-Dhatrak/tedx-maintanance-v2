16881db154a9a63de6138c5cc301d6e0b1f743bd
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2022-04-17 13:26:02 +0530

    header fix

--- src/pages/Home/CountdownSection/CountdownSection.jsx
+++ src/pages/Home/CountdownSection/CountdownSection.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./CountdownSection.scss";
-import image1 from "../../../images/Home/CountdownImg.png";
+// import image1 from "../../../images/Home/CountdownImg.png";
 import image2 from "../../../images/Home/speaker2.webp";
 import image3 from "../../../images/Home/speaker3.webp";
 
-const imageArray = [image1, image2, image3];
+const imageArray = [
+  "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1650021314/Website%20Content/Home/CountdownImg_ieaymp.png",
+  image2,
+  image3,
+];
 
 const CountdownSection = () => {
   const [current, setCurrent] = React.useState(0);
@@ -22,7 +26,9 @@ const CountdownSection = () => {
     <div className="countdown-container">
       <div className="subCountainerLf">
         <img src={imageArray[current]} alt="img" />
-        <p>Days to go, for the most <br /> awaited event of the year! </p>
+        <p>
+          Days to go, for the most <br /> awaited event of the year!{" "}
+        </p>
       </div>
       <div className="subCountainerRg">
         <h3>Embrace the Pandemonium on 21st May, 2022. </h3>

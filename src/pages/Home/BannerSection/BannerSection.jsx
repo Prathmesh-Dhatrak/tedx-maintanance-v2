0f1a31849ea013f7a549c22b15426e0ef9512069
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2022-04-20 17:24:26 +0530

    3D banner need to be added

--- src/pages/Home/BannerSection/BannerSection.jsx
+++ src/pages/Home/BannerSection/BannerSection.jsx
@@ -18,13 +18,13 @@ const BannerSection = () => {
         </p>
       </div>
       <div className="banner-section2">
-        {/* <iframe
+        <iframe
           src="https://my.spline.design/untitled-0895dc11d4d4f56fc2ff9039ba70f526/"
           frameborder="0"
           width="100%"
           height="100%"
           title="This is a unique title"
-        /> */}
+        />
       </div>
     </div>
   );

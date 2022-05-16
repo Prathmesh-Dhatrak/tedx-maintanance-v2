e5b67550c02a79f22fe7b177fa1ba48880151cf7
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2022-05-16 16:20:37 +0530

    countdown

--- src/pages/Home/CountdownSection/CountdownSection.jsx
+++ src/pages/Home/CountdownSection/CountdownSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./CountdownSection.scss";
 
 const imageArray = [
-  /*"https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509158/Website%20Content/Home/CountdownIMGS/19_settwi.svg",
+  "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509158/Website%20Content/Home/CountdownIMGS/19_settwi.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509143/Website%20Content/Home/CountdownIMGS/18_n0qly8.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509128/Website%20Content/Home/CountdownIMGS/17_u3lfhw.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509116/Website%20Content/Home/CountdownIMGS/16_llnl6x.svg",
@@ -10,7 +10,7 @@ const imageArray = [
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509094/Website%20Content/Home/CountdownIMGS/14_ya6hff.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509085/Website%20Content/Home/CountdownIMGS/13_ctsfzl.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509078/Website%20Content/Home/CountdownIMGS/12_h4kekm.svg",
-  "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509075/Website%20Content/Home/CountdownIMGS/11_tvspld.svg",*/
+  "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509075/Website%20Content/Home/CountdownIMGS/11_tvspld.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509073/Website%20Content/Home/CountdownIMGS/10_fkwuur.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509455/Website%20Content/Home/CountdownIMGS/9_cwsizw.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509314/Website%20Content/Home/CountdownIMGS/8_xlmdqh.svg",
@@ -22,27 +22,27 @@ const imageArray = [
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509175/Website%20Content/Home/CountdownIMGS/2_quvdpv.svg",
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1651509072/Website%20Content/Home/CountdownIMGS/1_v5gqm4.svg",
 ];
+imageArray.reverse();
 
 const CountdownSection = () => {
-  // const [current, setCurrent] = React.useState(0);
+  const [day, setday] = React.useState(0);
 
-  // React.useEffect(() => {
-  //   const timerId = setInterval(() => {
-  //     setCurrent((cur) => (cur < imageArray.length - 1 ? cur + 1 : 0));
-  //   }, 1000 * 60 * 60 * 24);
-  //   return () => {
-  //     clearInterval(timerId);
-  //   };
-  // }, []);
+  React.useEffect(() => {
+    var today = new Date();
+    var day = today.getDate();
+    day = 21 - day;
+    console.log(day);
+    setday(day);
+  }, []);
 
   return (
     <div className="countdown-container">
       <div className="subCountainerLf">
         <div className="img_container">
-          <img src={imageArray[5]} alt="img" />
+          <img src={imageArray[day -1]} alt="img" />
         </div>
         <p>
-          Days to go, for the most <br /> awaited event of the year!{" "}
+          Days to go, for the most <br /> awaited event of the year!
         </p>
       </div>
     </div>

f893423ce456e4790577e5c34bb27c2b129e1557
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2022-05-21 01:47:21 +0530

    ggg

--- src/pages/EventDetails/EventDetails.jsx
+++ src/pages/EventDetails/EventDetails.jsx
@@ -1,19 +1,97 @@
 import React from "react";
 import "./EventDetails.scss";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
-import TimelineDot from "@mui/lab/TimelineDot";
-import FastfoodIcon from "@mui/icons-material/Fastfood";
-import LaptopMacIcon from "@mui/icons-material/LaptopMac";
-import HotelIcon from "@mui/icons-material/Hotel";
-import RepeatIcon from "@mui/icons-material/Repeat";
-import Typography from "@mui/material/Typography";
+
 const grid =
   "https://res.cloudinary.com/dfgz4hsg0/image/upload/v1650021025/Website%20Content/WhiteGrid_xcidhq.svg";
+const timelineData = [
+  {
+    text: "Started working on the app-ideas repository",
+    date: "February 25 2019",
+    category: {
+      tag: "app-ideas",
+      color: "#FFDB14",
+    },
+    link: {
+      url: "#!",
+      text: "Check it out on GitHub",
+    },
+  },
+  {
+    text: "Started the Weekly Coding Challenge program",
+    date: "March 04 2019",
+    category: {
+      tag: "blog",
+      color: "#e17b77",
+    },
+    link: {
+      url: "#!",
+      text: "Check it out here",
+    },
+  },
+  {
+    text: "Got 1.000 followers on Twitter",
+    date: "March 07 2019",
+    category: {
+      tag: "twitter",
+      color: "#1DA1F2",
+    },
+    link: {
+      url: "#!",
+      text: "See profile",
+    },
+  },
+  {
+    text: "I published my first article in the FreeCodeCamp Medium Publication",
+    date: "March 18 2019",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+    link: {
+      url: "#!",
+      text: "Check it out here",
+    },
+  },
+  {
+    text: "Over 12.000 views in a single day on my Medium posts",
+    date: "April 05 2019",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+    link: {
+      url: "#!",
+      text: "See profile",
+    },
+  },
+];
+
+const TimelineItem = ({ data }) => (
+  <div className="timeline-item">
+    <div className="timeline-item-content">
+      <span className="tag" style={{ background: data.category.color }}>
+        {data.category.tag}
+      </span>
+      <time>{data.date}</time>
+      <p>{data.text}</p>
+      {data.link && (
+        <a href={data.link.url} target="_blank" rel="noopener noreferrer">
+          {data.link.text}
+        </a>
+      )}
+      <span className="circle" />
+    </div>
+  </div>
+);
+
+const Timeline = () =>
+  timelineData.length > 0 && (
+    <div className="timeline-container">
+      {timelineData.map((data, idx) => (
+        <TimelineItem data={data} key={idx} />
+      ))}
+    </div>
+  );
 
 const Map = ({ dark }) => {
   return (
@@ -29,83 +107,7 @@ const Map = ({ dark }) => {
           src="https://res.cloudinary.com/dfgz4hsg0/image/upload/v1652772322/Website%20Content/EventDetails/ComingSoon_mbp7w5.jpg"
           alt=""
         /> */}
-        <Timeline position="alternate">
-          <TimelineItem>
-            <TimelineOppositeContent
-              sx={{ m: "auto 0" }}
-              align="right"
-              variant="body2"
-              color="text.secondary"
-            >
-              9:30 am
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot>
-                <FastfoodIcon />
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: "12px", px: 2 }}>
-              <Typography variant="h6" component="span">
-                Eat
-              </Typography>
-              <Typography>Because you need strength</Typography>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineOppositeContent
-              sx={{ m: "auto 0" }}
-              variant="body2"
-              color="text.secondary"
-            >
-              10:00 am
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot color="primary">
-                <LaptopMacIcon />
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: "12px", px: 2 }}>
-              <Typography variant="h6" component="span">
-                Code
-              </Typography>
-              <Typography>Because it&apos;s awesome!</Typography>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot color="primary" variant="outlined">
-                <HotelIcon />
-              </TimelineDot>
-              <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: "12px", px: 2 }}>
-              <Typography variant="h6" component="span">
-                Sleep
-              </Typography>
-              <Typography>Because you need rest</Typography>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
-              <TimelineDot color="secondary">
-                <RepeatIcon />
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: "12px", px: 2 }}>
-              <Typography variant="h6" component="span">
-                Repeat
-              </Typography>
-              <Typography>Because this is the life you love!</Typography>
-            </TimelineContent>
-          </TimelineItem>
-        </Timeline>
+        <Timeline />
       </div>
     </div>
   );
